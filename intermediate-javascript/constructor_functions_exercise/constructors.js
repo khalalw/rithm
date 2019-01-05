c0434c40b538a22cbf3a428eacf739bdb0046099
Khalal Walker <khalal.walker@gmail.com>
2019-01-05 12:34:35 -0800

    complete Person constructor

---
+++ intermediate-javascript/constructor_functions_exercise/constructors.js
@@ -0,0 +1,6 @@
+function Person(firstName, lastName, favoriteColor, favoriteNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.favoriteColor = favoriteColor;
+  this.favoriteNumber = favoriteNumber;
+}
