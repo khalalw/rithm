cee5ee4372fe54a87d1f9180b9f9f1a2891e275d
Khalal Walker <khalal.walker@gmail.com>
2019-01-05 14:15:08 -0800

    implement array.map

---
+++ intermediate-javascript/prototypes_exercise/prototypes.js
@@ -0,0 +1,39 @@
+function Person(firstName, lastName, favoriteColor, favoriteNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.favoriteColor = favoriteColor;
+  this.favoriteNumber = favoriteNumber;
+  this.favoriteFoods = [];
+  this.family = [];
+}
+
+Person.prototype.fullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+Person.prototype.toString = function () {
+  return `${this.fullName()}, Favorite Color: ${this.favoriteColor}, Favorite Number: ${this.favoriteNumber}`;
+};
+
+Person.prototype.addToFamily = function (obj) {
+  let isFamily = this.family.some(function (element) {
+    return obj.firstName === element.firstName && obj.lastName === element.lastName;
+  });
+
+  if (obj instanceof Person && !isFamily) {
+    this.family.push(obj);
+  }
+
+  return this.family.length;
+};
+
+Array.prototype.map = function (func, index, array) {
+  let arr = [];
+  this.forEach(function (v, i, a) {
+    index = i;
+    array = a;
+    arr.push(func(v, index, array));
+  });
+
+  return arr;
+};
