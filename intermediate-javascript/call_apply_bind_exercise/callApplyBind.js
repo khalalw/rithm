007700a308cf29893750b6c97d979b4022c75835
Khalal Walker <khalal.walker@gmail.com>
2019-01-04 16:25:16 -0800

    passed tests for sumEvenArgs

--- intermediate-javascript/call_apply_bind_exercise/callApplyBind.js
+++ intermediate-javascript/call_apply_bind_exercise/callApplyBind.js
@@ -1,3 +1,15 @@
 function arrayFrom() {
   return [].slice.call(arguments);
 }
+
+function sumEvenArguments() {
+  let argsArray = [].slice.call(arguments);
+  let sum = 0;
+
+  argsArray.forEach(function (i) {
+    if (i % 2 === 0) {
+      sum += i;
+    }
+  });
+  return sum;
+}
