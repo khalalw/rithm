708eeb29fbc6fa67aa6e6cfca18b559b510fc972
Khalal Walker <khalal.walker@gmail.com>
2019-01-07 00:47:56 -0800

    finalize movie data

--- intermediate-javascript/asynchronous_js/starWars/starWars.js
+++ intermediate-javascript/asynchronous_js/starWars/starWars.js
@@ -1,36 +1,133 @@
 $(function () {
+
   // Use the Star Wars API to get all of the opening crawls for the movie.
-  function getAllOpeningCrawls() {
+  function getMovieData() {
+    let swArr = [];
+
     $.getJSON(`https://swapi.co/api/films/1/`)
       .then(function (data) {
-        console.log(data.opening_crawl);
-        return $.getJSON(`https://swapi.co/api/films/2/`)
+
+        let movieObj = {};
+        let planetsArr = data.planets;
+        movieObj.title = data.title;
+        movieObj.planets = [];
+        movieObj["opening crawl"] = data.opening_crawl;
+
+
+        planetsArr.forEach(function (v, i, arr) {
+          $.getJSON(v)
+            .then(function (data) {
+              movieObj.planets.push(data.name);
+            });
+        });
+        swArr.push(movieObj);
+        return $.getJSON(`https://swapi.co/api/films/2/`);
       })
       .then(function (data) {
-        console.log(data.opening_crawl);
-        return $.getJSON(`https://swapi.co/api/films/3/`)
+        let movieObj = {};
+        let planetsArr = data.planets;
+        movieObj.title = data.title;
+        movieObj.planets = [];
+        movieObj["opening crawl"] = data.opening_crawl;
+
+
+        planetsArr.forEach(function (v, i, arr) {
+          $.getJSON(v)
+            .then(function (data) {
+              movieObj.planets.push(data.name);
+            });
+        });
+        swArr.push(movieObj);
+        return $.getJSON(`https://swapi.co/api/films/3/`);
       })
       .then(function (data) {
-        console.log(data.opening_crawl);
-        return $.getJSON(`https://swapi.co/api/films/4/`)
+        let movieObj = {};
+        let planetsArr = data.planets;
+        movieObj.title = data.title;
+        movieObj.planets = [];
+        movieObj["opening crawl"] = data.opening_crawl;
+
+
+        planetsArr.forEach(function (v, i, arr) {
+          $.getJSON(v)
+            .then(function (data) {
+              movieObj.planets.push(data.name);
+            });
+        });
+        swArr.push(movieObj);
+        return $.getJSON(`https://swapi.co/api/films/4/`);
       })
       .then(function (data) {
-        console.log(data.opening_crawl);
-        return $.getJSON(`https://swapi.co/api/films/5/`)
+        let movieObj = {};
+        let planetsArr = data.planets;
+        movieObj.title = data.title;
+        movieObj.planets = [];
+        movieObj["opening crawl"] = data.opening_crawl;
+
+
+        planetsArr.forEach(function (v, i, arr) {
+          $.getJSON(v)
+            .then(function (data) {
+              movieObj.planets.push(data.name);
+            });
+        });
+        swArr.push(movieObj);
+        return $.getJSON(`https://swapi.co/api/films/5/`);
       })
       .then(function (data) {
-        console.log(data.opening_crawl);
-        return $.getJSON(`https://swapi.co/api/films/6/`)
+        let movieObj = {};
+        let planetsArr = data.planets;
+        movieObj.title = data.title;
+        movieObj.planets = [];
+        movieObj["opening crawl"] = data.opening_crawl;
+
+
+        planetsArr.forEach(function (v, i, arr) {
+          $.getJSON(v)
+            .then(function (data) {
+              movieObj.planets.push(data.name);
+            });
+        });
+        swArr.push(movieObj);
+        return $.getJSON(`https://swapi.co/api/films/6/`);
       })
       .then(function (data) {
-        console.log(data.opening_crawl);
-        return $.getJSON(`https://swapi.co/api/films/7/`)
+        let movieObj = {};
+        let planetsArr = data.planets;
+        movieObj.title = data.title;
+        movieObj.planets = [];
+        movieObj["opening crawl"] = data.opening_crawl;
+
+
+        planetsArr.forEach(function (v, i, arr) {
+          $.getJSON(v)
+            .then(function (data) {
+              movieObj.planets.push(data.name);
+            });
+        });
+        swArr.push(movieObj);
+        return $.getJSON(`https://swapi.co/api/films/7/`);
       })
       .then(function (data) {
-        console.log(data.opening_crawl);
+        let movieObj = {};
+        let planetsArr = data.planets;
+        movieObj.title = data.title;
+        movieObj.planets = [];
+        movieObj["opening crawl"] = data.opening_crawl;
+
+
+        planetsArr.forEach(function (v, i, arr) {
+          $.getJSON(v)
+            .then(function (data) {
+              movieObj.planets.push(data.name);
+            });
+        });
+        swArr.push(movieObj);
       });
+    console.log(swArr);
+    return swArr;
 
   }
 
-  getAllOpeningCrawls();
+  getMovieData();
 });
