16f0c1524c1a2e1135b50a4c93f01ff3127e49e1
Khalal Walker <khalal.walker@gmail.com>
2019-01-08 22:56:24 -0800

    finish part 3 of jsx exercise

---
+++ react/02_jsx_introduction/part3/app.jsx
@@ -0,0 +1,47 @@
+class Person extends React.Component {
+  render() {
+    let age = this.props.age;
+    let hobbies = this.props.hobbies;
+    let name = this.props.name;
+    return (
+      <div>
+        <p>Learn some information about this person.</p>
+        <div>
+          {name} is {age} years old.
+        </div>
+        <div>Some of their hobbies include:</div>
+        <ul>
+          {hobbies.map(element => (
+            <li>{element}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id="people">
+        <Person
+          name="Khalal"
+          age="24"
+          hobbies={['running', 'coding', 'laughing', 'poetry', 'rapping']}
+        />
+        <Person
+          name="Tahvia"
+          age="24"
+          hobbies={['shopping', 'laughing', 'rapping']}
+        />
+        <Person
+          name="Rochelle"
+          age="53"
+          hobbies={['going to the gym', 'praying', 'singing']}
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
