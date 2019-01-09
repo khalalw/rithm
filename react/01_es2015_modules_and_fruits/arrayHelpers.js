a6de94a69700d32d09712c751548a7a095edca8c
Khalal Walker <khalal.walker@gmail.com>
2019-01-08 17:27:42 -0800

    update arrayHelpers

--- react/01_es2015_modules_and_fruits/arrayHelpers.js
+++ react/01_es2015_modules_and_fruits/arrayHelpers.js
@@ -1,10 +1,15 @@
 export function choice(arr) {
-  return arr[Math.floor(Math.random * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
 export function remove(arr, item) {
   let array = arr.slice();
   let idx = array.indexOf(item);
 
-  return idx < 0 ? undefined : array.splice(idx, 1);
+  if (idx >= 0) {
+    array.splice(idx, 1);
+    return array;
+  }
+
+  return undefined;
 }
