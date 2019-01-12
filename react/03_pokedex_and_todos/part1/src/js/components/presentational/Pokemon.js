76aa204c68bb51422eef17f66daf994ea0ebe665
Khalal Walker <khalal.walker@gmail.com>
2019-01-11 19:31:56 -0800

    update styling for pokeCards

--- react/03_pokedex_and_todos/part1/src/js/components/presentational/Pokemon.js
+++ react/03_pokedex_and_todos/part1/src/js/components/presentational/Pokemon.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+const cardStyle = {
+  backgroundColor: "lightGrey",
+  width: "200px",
+  textAlign: "center",
+  borderRadius: "25px",
+  paddingTop: "12px",
+  paddingBottom: "15px",
+};
+
 export default class Pokemon extends Component {
   render() {
     return (
       <li>
-        <h2>{this.props.name}</h2>
-        <img src={this.props.image} />
-        <p>
-          <strong>Type: </strong>
-          {this.props.type}
-        </p>
+        <div style={cardStyle} className="pokemon">
+          <h2>{this.props.name}</h2>
+          <img src={this.props.image} />
+          <p>
+            <strong>Type: </strong>
+            {this.props.type}
+          </p>
+        </div>
       </li>
     );
   }
