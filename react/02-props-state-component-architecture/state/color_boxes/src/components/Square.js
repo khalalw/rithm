134c34335cc05e15c9850f44e35d7fa65edf0e92
Khalal Walker <khalal.walker@gmail.com>
2019-01-13 22:46:49 -0800

    add state array

--- react/02-props-state-component-architecture/state/color_boxes/src/components/Square.js
+++ react/02-props-state-component-architecture/state/color_boxes/src/components/Square.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 
 class Square extends Component {
+  render() {
+    return <div class="square" />;
+  }
+}
+
+class App extends Component {
   constructor(props) {
     super(props);
-  }
-
-  render() {
-    return ();
+    this.state = {
+      colors: ["red", "orange", "yellow", "blue", "indigo", "violet"],
+    };
   }
 }
