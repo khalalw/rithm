43ffd847a56da9d035588e07da0bda210b441ae7
Khalal Walker <khalal.walker@gmail.com>
2019-01-16 20:54:10 -0800

    add solution for delete functionality

--- react/02-props-state-component-architecture/react-components/todo-app/src/components/Todo.js
+++ react/02-props-state-component-architecture/react-components/todo-app/src/components/Todo.js
@@ -1,10 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Todo = props => <li>{props.item}</li>;
+const Todo = props => (
+  <div>
+    <h4>{props.item}</h4>
+    <li>{props.title}</li>
+    <button onClick={props.complete}>Complete</button>
+    <button onClick={props.removeItem}>Delete</button>
+  </div>
+);
 
 Todo.propTypes = {
   item: PropTypes.string,
+  title: PropTypes.string,
 };
 
 export default Todo;
--- react/02-props-state-component-architecture/react-components/todo-app/src/components/TodoList.js
+++ react/02-props-state-component-architecture/react-components/todo-app/src/components/TodoList.js
@@ -5,13 +5,33 @@ class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: ["go to the store", "go to work"],
+      todos: ["go to the store", "get nails done"],
+      description: [
+        "need to grab groceries for the children",
+        "2PM appt at Gaia Nails",
+      ],
     };
+  }
+
+  handleRemove(index) {
+    const { todos, description } = this.state;
+    const newTodos = todos.slice(0, index).concat(todos.slice(index + 1));
+    const newDescription = description
+      .slice(0, index)
+      .concat(description.slice(index + 1));
+    this.setState({ todos: newTodos, description: newDescription });
   }
 
   render() {
     const listItems = this.state.todos.map((item, index) => {
-      return <Todo key={index} item={item} />;
+      return (
+        <Todo
+          key={index}
+          item={item}
+          removeItem={this.handleRemove.bind(this, index)}
+          title={this.state.description[index]}
+        />
+      );
     });
 
     return (
