c963de1aa1edbc6716f85660defc64760489e826
Khalal Walker <khalal.walker@gmail.com>
2019-01-30 19:24:49 -0800

    implement selection sort

--- javascript_computer_science_exercises-master/sorting_algorithms_exercise/sorting.js
+++ javascript_computer_science_exercises-master/sorting_algorithms_exercise/sorting.js
@@ -15,3 +15,26 @@ function bubbleSort(arr) {
 
   return arr;
 }
+
+// Selection sort
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = arr[i];
+    let minIndex = 0;
+    let swapped = false;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < min) {
+        min = arr[j];
+        minIndex = j;
+        swapped = true;
+      }
+    }
+    if (swapped) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
+}
