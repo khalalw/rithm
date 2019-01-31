6437f0fb3b1da9d228c60e98334c3ccbcd65cd53
Khalal Walker <khalal.walker@gmail.com>
2019-01-30 19:26:32 -0800

    implement merge sort

--- javascript_computer_science_exercises-master/sorting_algorithms_exercise/sorting.js
+++ javascript_computer_science_exercises-master/sorting_algorithms_exercise/sorting.js
@@ -52,3 +52,28 @@ function insertionSort(arr) {
 
   return arr;
 }
+
+// Merge Sort
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let middle = Math.ceil(arr.length / 2);
+  let left = arr.slice(0, middle);
+  let right = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  return result.concat(left.length ? left : right);
+}
