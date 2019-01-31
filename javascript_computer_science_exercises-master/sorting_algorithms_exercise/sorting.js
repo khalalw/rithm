7fb2e7c4081fe4169bcd741fa88f4df42a4ea22a
Khalal Walker <khalal.walker@gmail.com>
2019-01-30 19:24:13 -0800

    implement bubble sort

---
+++ javascript_computer_science_exercises-master/sorting_algorithms_exercise/sorting.js
@@ -0,0 +1,17 @@
+// Bubble Sort
+function bubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return arr;
+}
