120990bdc8e02ddbc010b711392d7b266ea6a417
Khalal Walker <khalal.walker@gmail.com>
2019-01-31 18:26:17 -0800

    update selection variable

--- javascript_computer_science_exercises-master/sorting_algorithms_exercise/sorting.js
+++ javascript_computer_science_exercises-master/sorting_algorithms_exercise/sorting.js
@@ -21,16 +21,16 @@ function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let min = arr[i];
     let minIndex = 0;
-    let swapped = false;
+    let isNewMinimum = false;
 
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < min) {
         min = arr[j];
         minIndex = j;
-        swapped = true;
+        isNewMinimum = true;
       }
     }
-    if (swapped) {
+    if (isNewMinimum) {
       let temp = arr[i];
       arr[i] = arr[minIndex];
       arr[minIndex] = temp;
@@ -41,10 +41,11 @@ function selectionSort(arr) {
 
 // Insertion sort
 function insertionSort(arr) {
+  // mark first element as sorted
   for (let i = 1; i < arr.length; i++) {
+    // extract element for each unsorted element
     let temp = arr[i];
-
-    for (let j = i - 1; j >= 0 && arr[i] < arr[j]; j--, i--) {
+    for (let j = i - 1; j >= 0 && temp < arr[j]; j--, i--) {
       arr[i] = arr[j];
       arr[j] = temp;
     }
@@ -55,9 +56,7 @@ function insertionSort(arr) {
 
 // Merge Sort
 function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
+  if (arr.length <= 1) return arr;
 
   let middle = Math.ceil(arr.length / 2);
   let left = arr.slice(0, middle);
